c5c5dc3a7da21100ce1e73ca26f1ce11e7707739
DavidPerezContreras <91068383+DavidPerezContreras@users.noreply.github.com>
2021-11-01 00:52:12 +0100

    Funciona, identado mejorado

--- MainQuitarVocales.java
+++ MainQuitarVocales.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.util.Scanner;
 import java.io.*;
+
+//Pide una palabra por el teclado, hay una tuberia de entrada y otra de salida
 public class MainQuitarVocales {
     public static void main(String[] args) {
         String palabra="";
@@ -17,7 +19,7 @@ public class MainQuitarVocales {
 
             Scanner tecladoPalabra = new Scanner(System.in);
             palabra=tecladoPalabra.nextLine();
-
+            tecladoPalabra.close();
 
 
 
@@ -30,10 +32,11 @@ public class MainQuitarVocales {
             //Recibe la salida estandar del proceso p
             Scanner scQuitarVocales = new Scanner(p.getInputStream());
                 palabraSinVocales=scQuitarVocales.nextLine();
+                scQuitarVocales.close();
                 System.out.print(palabraSinVocales);
                 
-            scQuitarVocales.close();
-            tecladoPalabra.close();
+            
+            
 
 
 
