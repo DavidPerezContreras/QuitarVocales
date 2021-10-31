9a4e6b01067aba35ddb86aa7aa7e6c0fa933a0c4
DavidPerezContreras <91068383+DavidPerezContreras@users.noreply.github.com>
2021-11-01 00:25:51 +0100

    Update MainQuitarVocales.java
    Funciona, El subproceso recibe la salida del teclado del main

--- MainQuitarVocales.java
+++ MainQuitarVocales.java
@@ -1,22 +1,41 @@
 import java.io.IOException;
 import java.util.Scanner;
-
+import java.io.*;
 public class MainQuitarVocales {
     public static void main(String[] args) {
-        
-        final String palabra="David Perez Contreras";
+        String palabra="";
+        String palabraSinVocales="";
 
-        ProcessBuilder pb = new ProcessBuilder("java","QuitarVocales",palabra);
+        ProcessBuilder pb = new ProcessBuilder("java","QuitarVocales");
         try {
             Process p = pb.start();
             
-            //
-            Scanner sc = new Scanner(p.getInputStream());
-            
-            
-                System.out.print(sc.nextLine());
+
+            //Pide una palabra por el teclado
+            System.out.println("Introduce una palabra.");
+
+
+            Scanner tecladoPalabra = new Scanner(System.in);
+            palabra=tecladoPalabra.nextLine();
+
+
+
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+                bw.write(palabra);//BufferedWriter me permite indicarle el String directamente.
+            bw.close();
+
+
+
+            //Recibe la salida estandar del proceso p
+            Scanner scQuitarVocales = new Scanner(p.getInputStream());
+                palabraSinVocales=scQuitarVocales.nextLine();
+                System.out.print(palabraSinVocales);
                 
-            sc.close();
+            scQuitarVocales.close();
+            tecladoPalabra.close();
+
+
 
 
         } catch (IOException e) {
